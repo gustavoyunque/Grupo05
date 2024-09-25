5669c24666b6a20051fd89f724952bc822cd7676
ypss852 <falvarezlla@unsa.edu.pe>
2024-09-24 21:40:04 -0500

    Se implementa el primer metodo para generar el titulo

---
+++ src/pweb2/HTMLGenerator.java
@@ -0,0 +1,39 @@
+package pweb2;
+
+public class HTMLGenerator {
+
+    //título
+	public String createTitle() {
+        String title = "Nuestro primer proyecto en GitHub";
+        return "<title>" + title + "</title>";
+    }
+    //encabezado h1
+    public String createHeader(String header, int level) {
+        return ""; 
+    }
+
+    //párrafo
+    public String createParagraph(String text) {
+        return "";
+    }
+
+    //(hipervínculo)
+    public String createLink(String url, String text) {
+        return ""; 
+    }
+
+    // list ordenada o no ordenada
+    public String createList(String[] items, boolean ordered) {
+        return ""; 
+    }
+
+    // Método principal 
+    public static void main(String[] args) {
+        
+}
+
+
+
+
+
+}
