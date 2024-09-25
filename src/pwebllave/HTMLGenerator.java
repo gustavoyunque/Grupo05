f4c906254e71ec89e2f8b3dd5c2945cc68f81b8f
JafetLlave <jllaveag@unsa.edu.pe>
2024-09-24 23:14:41 -0500

    Se agrega el cuarto método que permite agregar un hiperenlace

---
+++ src/pwebllave/HTMLGenerator.java
@@ -0,0 +1,45 @@
+package pwebllave;
+
+
+public class HTMLGenerator {
+
+    //título
+	public String createTitle() {
+        String title = "Nuestro primer proyecto en GitHub";
+        return "<title>" + title + "</title>";
+    }
+
+	public String createHeader() {
+        String header = "¡Aprender GitHub con amigos es muy divertido!";
+        int level = 1; 
+        return "<h" + level + ">" + header + "</h" + level + ">";
+    }
+
+	public String createParagraph() {
+        String text = "Nosotros somos el grupo 05 y hemos usado Git para poder hacer pull y push según corresponda. "
+                    + "De esta manera podemos tener un mejor control de versiones al momento de hacer proyectos en grupo.";
+        return "<p>" + text + "</p>";
+    }
+
+    //(hipervínculo)
+    public String createLink() {
+        String url = "https://github.com/gustavoyunque/Grupo05";
+        String text = "Este es el enlace a nuestro repositorio GitHub";
+        return "<a href=\"" + url + "\">" + text + "</a>";
+    }
+
+   // list ordenada o no ordenada
+    	    public String createList(String[] items, boolean ordered) {
+        return ""; 
+    }
+
+    // Método principal 
+    public static void main(String[] args) {
+
+}
+
+
+
+
+
+}
