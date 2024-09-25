19856ccd8c33e855b13093e9c0e4bc677d7bd0a8
roydanpe <rapazaan@unsa.edu.pe>
2024-09-24 22:36:53 -0500

    se  agrega el tercer metodo para agregar un parrafo

---
+++ src/pweb/HTMLGenerator.java
@@ -0,0 +1,42 @@
+package pweb;
+
+
+public class HTMLGenerator {
+
+    //título
+	public String createTitle() {
+        String title = "Nuestro primer proyecto en GitHub";
+        return "<title>" + title + "</title>";
+    }
+
+	public String createHeader() {
+        String header = "¡Aprender GitHub con amigos es muy divertido!";
+        int level = 1; 
+        return "<h" + level + ">" + header + "</h" + level + ">";
+    }
+
+    //párrafo
+    public String createParagraph(String text) {
+        return "";
+    }
+
+    //(hipervínculo)
+    public String createLink(String url, String text) {
+        return ""; 
+    }
+
+   // list ordenada o no ordenada
+    	    public String createList(String[] items, boolean ordered) {
+        return ""; 
+    }
+
+    // Método principal 
+    public static void main(String[] args) {
+        
+}
+
+
+
+
+
+}
